baf83fb3928fe06447179353ce9e6968bdbc7da3
David-Andrei Viziteu <31017005+davidviziteu@users.noreply.github.com>
2021-12-28 00:13:14 +0200

    renamed a variable; added frame types

--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 import struct
 
+IPV4_FRAME = 0x0800
+IPV6_FRAME = 0x86dd
+
 def get_readable_mac(bytes_mac):
     return bytes_mac.hex(':')
 
 def unpack_ethernet_frame(ethernet_frame):
     macs_and_proto = ethernet_frame[:14]
-    dest_mac, source_mac, protocol = struct.unpack('! 6s 6s H', macs_and_proto)
+    dest_mac, source_mac, frame_type = struct.unpack('! 6s 6s H', macs_and_proto)
     dest_mac = get_readable_mac(dest_mac)
     source_mac = get_readable_mac(source_mac)
-    return dest_mac, source_mac, protocol, ethernet_frame[14:]
+    return dest_mac, source_mac, frame_type, ethernet_frame[14:]
 
