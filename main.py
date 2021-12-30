b582382fb5f4df9e27001ec90e596f17bdd4067f
David-Andrei Viziteu <31017005+davidviziteu@users.noreply.github.com>
2021-12-30 23:35:08 +0200

    implemented filters for requests; refactoring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from utils import *
 import socket
-import protocol_types
-import ethernet_frame_types
 import os
 import argparse
 
@@ -11,14 +9,31 @@ except ImportError:
     from http_parser.pyparser import HttpParser
 
 
-def check_filters(filters, device, src_mac, src_ip, src_port, dest_mac, dest_ip, http_data):
+def print_headers(n_tabs, headers):
+    """nicely prints headers from HttpParser instance with the given tabs"""
+    for key, value in headers.items():
+        print(tabs(n_tabs) + f'{key}: {value}')
+
+
+def check_filters(filters, device, src_mac, src_ip, dest_mac, dest_ip, http_data: HttpParser):
     """checks whether or not the given tcp packet is subject to the applied filters or not"""
-    pass
+    if filters.filter_from_ip and src_ip not in filters.filter_from_ip:
+        return False
+    if filters.filter_to_ip and dest_ip not in filters.filter_to_ip:
+        return False
+    if filters.filter_from_mac and src_mac not in filters.filter_from_mac:
+        return False
+    if filters.filter_to_mac and dest_mac not in filters.filter_to_mac:
+        return False
+    if filters.filter_http_verb and http_data.get_method() not in filters.filter_http_verb:
+        return False
+    if filters.filter_device and device not in filters.filter_device:
+        return False
+    return True
 
 
-def main():
+def main(filters):
     socket_conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-    httpParser = HttpParser()
     while 1:
         raw_eth_frame, other_data = socket_conn.recvfrom(60000)
         dest_mac, src_mac, ip_version, eth_payload = unpack_ethernet_frame(raw_eth_frame)
@@ -26,10 +41,8 @@ def main():
 
         if ip_version == 4:
             protocol, src_ip, dest_ip, ipvX_payload = unpack_ipv4_frame(eth_payload)
-            ip_version = 'IPV4'
         elif ip_version == 6:
             protocol, src_ip, dest_ip, ipvX_payload = unpack_ipv6_frame(eth_payload)
-            ip_version = 'IPV6'
         else:
             continue
 
@@ -43,20 +56,34 @@ def main():
             continue
         if 'HTTP' not in decoded_tcp_payload:
             continue
+        httpParser = HttpParser()
+        payload_len = len(tcp_payload)
+        parsed_len = httpParser.execute(tcp_payload, payload_len)
+
+        if len(tcp_payload) != parsed_len:
+            print(f'tcp payload http parsing failed. parsed len = {parsed_len} payload len = {payload_len}')
+            continue
+
+        if not check_filters(filters, physical_device, src_mac, src_ip, dest_mac, dest_ip, httpParser):
+            continue
+
+        try:
+            decoded_http_req_body = httpParser.recv_body().decode("utf8")
+        except UnicodeDecodeError:
+            decoded_http_req_body = 'Unable to decode http body as it contains non utf8 characters'
 
         print(tabs(0) + f'ETHERNET FRAME - device: {physical_device}')
-        print(tabs(1) + f'frame_type: {ip_version}')
+        print(tabs(1) + f'ip version: {ip_version}')
         print(tabs(1) + f'TCP FRAME:')
         print(tabs(2) + f'src: {src_mac} - {src_ip} : {src_port}')
         print(tabs(2) + f'dst: {dest_mac} - {dest_ip} : {dest_port}')
         print(tabs(2) + f'sequence: {sequence}')
-
-        len_parsed = httpParser.execute(tcp_payload, len(tcp_payload))
-        if len(tcp_payload) == len_parsed:
-            print(tabs(2) + f'tcp payload:')
-            print(tabs(2) + f'headers: {httpParser.get_headers()}')
-        else:
-            print(tabs(2) + f'tcp payload http parsing failed')
+        print(tabs(2) + f'tcp payload:')
+        print(tabs(3) + f'method: {httpParser.get_method()}')
+        print(tabs(3) + f'headers:')
+        print_headers(4, httpParser.get_headers())
+        if httpParser.get_method() != 'GET' and httpParser.get_method() != 'HEAD':
+            print(tabs(3) + f'body: {decoded_http_req_body}')
         print()
 
 
@@ -64,6 +91,9 @@ if __name__ == '__main__':
     if os.geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
     parser = argparse.ArgumentParser(description='Sniff http requests leaving and coming to this machine')
+    parser.add_argument('--device', type=str, dest='filter_device',
+                        help='filter only http requests related to physical device',
+                        action='append')
     parser.add_argument('--from-ip', type=str, dest='filter_from_ip',
                         help='filter only http requests coming from ip address',
                         action='append')
@@ -77,7 +107,5 @@ if __name__ == '__main__':
                         action='append')
     parser.add_argument('--http-verb', type=str, dest='filter_http_verb',
                         help='filter only http that have the respective verb', action='append')
-    parser.add_argument('--contain-text', type=str, dest='filter_contain_text',
-                        help='filter only http that contain text', action='append')
     args = parser.parse_args()
-    main()
+    main(args)
