64514a6c8f2909f7d4ef51390ca1382fa79d872b
David-Andrei Viziteu <31017005+davidviziteu@users.noreply.github.com>
2021-12-30 22:18:19 +0200

    fixed last commit rebase issues; added http parsing of tcp payload

--- main.py
+++ main.py
@@ -5,47 +5,64 @@ import ethernet_frame_types
 import os
 import argparse
 
+try:
+    from http_parser.parser import HttpParser
+except ImportError:
+    from http_parser.pyparser import HttpParser
+
+
+def check_filters(filters, device, src_mac, src_ip, src_port, dest_mac, dest_ip, http_data):
+    """checks whether or not the given tcp packet is subject to the applied filters or not"""
+    pass
+
 
 def main():
     socket_conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    httpParser = HttpParser()
     while 1:
         raw_eth_frame, other_data = socket_conn.recvfrom(60000)
-        dest_mac, src_mac, frame_type, eth_payload = unpack_ethernet_frame(raw_eth_frame)
+        dest_mac, src_mac, ip_version, eth_payload = unpack_ethernet_frame(raw_eth_frame)
         physical_device = other_data[0]
 
-        if frame_type == ethernet_frame_types.IPV4:
-            protocol, src_ip, dest_ip, ipv4_payload = unpack_ipv4_frame(eth_payload)
-            frame_type = 'IPV4'
-        elif frame_type == ethernet_frame_types.IPV6:
-            protocol, src_ip, dest_ip, ipv4_payload = unpack_ipv6_frame(eth_payload)
-            frame_type = 'IPV6'
+        if ip_version == 4:
+            protocol, src_ip, dest_ip, ipvX_payload = unpack_ipv4_frame(eth_payload)
+            ip_version = 'IPV4'
+        elif ip_version == 6:
+            protocol, src_ip, dest_ip, ipvX_payload = unpack_ipv6_frame(eth_payload)
+            ip_version = 'IPV6'
         else:
             continue
 
-        src_port, dest_port, sequence, tpc_payload = unpack_tcp_frame(ipv4_payload)
+        src_port, dest_port, sequence, tcp_payload = unpack_tcp_frame(ipvX_payload)
         if dest_port != 80:
             continue
         try:
-            tcp_payload = tpc_payload.decode("utf8")
+            decoded_tcp_payload = tcp_payload.decode("utf8")
         except UnicodeDecodeError:
             print('Unable to decode tcp payload as it contains non utf8 characters')
             continue
-        if 'HTTP' not in tcp_payload:
+        if 'HTTP' not in decoded_tcp_payload:
             continue
+
         print(tabs(0) + f'ETHERNET FRAME - device: {physical_device}')
-        print(tabs(1) + f'frame_type: {frame_type}')
+        print(tabs(1) + f'frame_type: {ip_version}')
         print(tabs(1) + f'TCP FRAME:')
         print(tabs(2) + f'src: {src_mac} - {src_ip} : {src_port}')
         print(tabs(2) + f'dst: {dest_mac} - {dest_ip} : {dest_port}')
         print(tabs(2) + f'sequence: {sequence}')
-        print(tabs(2) + f'===========tpc_payload===========\n{tcp_payload}')
-        print(tabs(2) + f'=================================')
+
+        len_parsed = httpParser.execute(tcp_payload, len(tcp_payload))
+        if len(tcp_payload) == len_parsed:
+            print(tabs(2) + f'tcp payload:')
+            print(tabs(2) + f'headers: {httpParser.get_headers()}')
+        else:
+            print(tabs(2) + f'tcp payload http parsing failed')
         print()
 
 
 if __name__ == '__main__':
-    # if os.geteuid() != 0:
-    #     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+    if os.geteuid() != 0:
+        exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
     parser = argparse.ArgumentParser(description='Sniff http requests leaving and coming to this machine')
     parser.add_argument('--from-ip', type=str, dest='filter_from_ip',
                         help='filter only http requests coming from ip address',
