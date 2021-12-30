e1092c1961af6783ae57fb1dd3b0ba24078e0b53
David-Andrei Viziteu <31017005+davidviziteu@users.noreply.github.com>
2021-12-30 12:29:36 +0200

    unpacking ipv4 frame

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
+import ipaddress
 import struct
 
 IPV4_FRAME = 0x0800
 IPV6_FRAME = 0x86dd
+
+TCP_PROTO_ID = 6
+
+def tabs(n):
+    return '\t' * n
 
 def get_readable_mac(bytes_mac):
     return bytes_mac.hex(':')
@@ -13,3 +19,11 @@ def unpack_ethernet_frame(ethernet_frame):
     source_mac = get_readable_mac(source_mac)
     return dest_mac, source_mac, frame_type, ethernet_frame[14:]
 
+def unpack_ipv4_frame(data):
+    version_length = data[0]
+    version = version_length >> 4
+    header_length = (version_length & 15) * 4
+    ttl, protocol, src_ipv4, dest_ipv4 = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    src_ipv4 = str(ipaddress.ip_address(src_ipv4))
+    dest_ipv4 = str(ipaddress.ip_address(dest_ipv4))
+    return protocol, src_ipv4, dest_ipv4, data[header_length:]
